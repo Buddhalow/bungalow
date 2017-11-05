8ba7db33dd5212a274c2dcbb98b7d4ddfec03e9d
Dr. Sounds <management@dr-sounds.com>
2017-11-05 11:01:35 +0000

    T

--- client/js/controls/chrome.js
+++ client/js/controls/chrome.js
@@ -171,7 +171,8 @@ define(
             this.mainView = document.createElement('sp-main');
             this.main.appendChild(this.mainView);
             this.rightSidebar = document.createElement('sp-feedview');
-            this.rightSidebar.setAttribute('hidden', true);
+            this.rightSidebar.style.width = '10%';
+            //this.rightSidebar.setAttribute('hidden', true);
             this.main.appendChild(this.rightSidebar);
             this.appFooter = document.createElement('sp-appfooter');
             this.appendChild(this.appFooter);
