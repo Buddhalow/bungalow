414aff2638c9ef8242b83348ef9156896d5d8efb
Dr. Sounds <management@dr-sounds.com>
2017-12-20 13:37:51 +0000

    %s

--- sync.sh
+++ sync.sh
@@ -3,4 +3,4 @@ git commit -m "%s"
 git push
 git submodule foreach "git add --all :// || true"
 git submodule foreach "git add commit -m '%s' || true"
-git submodule foreach "git add push || true"
+git submodule foreach "git push || true"
